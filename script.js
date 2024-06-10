1c05ae1d8050f9651d492b7160d44fb469d81bb8
tanishkkkaa <tanuagarwaltanuagarwal@gmail.com>
2024-06-10 13:23:12 +0530

    weather app

---
+++ script.js
@@ -0,0 +1,22 @@
+const button = document.getElementById("search-button");
+const input = document.getElementById("city-input");
+const cityName = document.getElementById("city-name");
+cityName.textContent = cityName.textContent.toLowerCase();
+const cityTemp = document.getElementById("city-temp");
+const cityTime = document.getElementById("city-time");
+
+async function getData(cityName){
+    const promise = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=8e0cc6420b37403b9e461037242104&q=${cityName}&aqi=yes`
+    );
+    return await promise.json();
+};
+
+button.addEventListener("click", async () => {
+    const value =  input.value;
+    const result = await getData(value);
+    cityName.innerText = `${result.location.name}, ${result.location.region}, ${result.location.country}`;
+    cityTime.innerText = result.location.localtime;
+    cityTemp.innerText = result.current.temp_c;
+    console.log(result);
+});
